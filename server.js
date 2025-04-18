a7aa2730626c1fe56d33cebb48d3ce9d745a629c
Gabriel de Carvalho <g4brieldcs@gmail.com>
2025-04-17 21:09:10 -0300

    adição do banco de dados e função de receber dados, faltando apenas configurar o armazenamento de imagens

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const conexao = mysql.createConnection({
     host: "localhost",
     user:"root",
     password:"root",
-    database:"Vestidos"
+    database:"vestido"
 })
 
 conexao.connect((erro) => {
@@ -22,10 +22,22 @@ conexao.connect((erro) => {
 let vestidos = []
 
 app.post('/vestidos' , (req, res) => {
+    let codigoVestido = req.body.cod_vestido;
+    let nomeVestido = req.body.nome;
+    let imagem = req.file.imagem.name;
     
-    console.log(req)
-    vestidos.push(req.body);
-    res.status(201).json({message :"vestido enviado com sucesso"})
+    let sql = `INSERT INTO vestidos (codigo_vestido, nome_vestido, imagem) VALUES ('${codigoVestido}', '${nomeVestido}','${imagem}')`;
+    
+    conexao.query(sql, (erro, callback) => {
+        
+        if(erro) throw erro;
+        
+        console.log(callback)
+        
+    })
+    
+    res.redirect('/');
+    
 })
 
 app.get('/vestidos', (req,res) => {
@@ -34,5 +46,5 @@ app.get('/vestidos', (req,res) => {
 
 
 app.listen(port, () => {
-    console.log("Servidor rodando em http://localhost:3000")
+    console.log("Servidor rodando em http://localhost:3001")
 })
